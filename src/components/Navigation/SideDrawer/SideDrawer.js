e160728660159351fc6a12adfc10b91f80f773ba
Pankaj K S <SPankaj@ESTUATE.COM>
2019-07-01 20:53:27 +0530

    SideDrawer backdrop clicker done

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,10 +6,16 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import classes from './SideDrawer.module.css';
 
 const sideDrawer = (props) => {
+
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
         <>
             <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={classes.SideDrawer}>
+            <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
                     <Logo sideDrawer />
                 </div>
