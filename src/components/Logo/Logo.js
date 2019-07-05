59cf8d6ffed436820691d249a449c4356f067f11
Pankaj K S <SPankaj@ESTUATE.COM>
2019-07-05 19:29:59 +0530

    The componentWillUpdate is not implemented yet. Once done the module will be complete

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,14 +6,21 @@ import classes from './Logo.module.css';
 const logo = (props) => {
     let ele = <img src={burgerLogo} alt="MyBurger" />;
     if (!props.sideDrawer) {
-        ele = <a href="https://www.google.co.in/" target="_blank"  rel="noopener noreferrer"><img src={burgerLogo} alt="MyBurger" /></a>;
+        ele = <a href="https://www.google.co.in/" target="_blank"  rel="noopener noreferrer">{ele}</a>;
+    }
+
+    let totalClasses = null;
+    if(props.white) {
+        totalClasses = [classes.Logo, classes.White].join(' ');
+    } else {
+        totalClasses = classes.Logo
     }
 
     return (
-        <div className={classes.Logo}>
+        <div className={totalClasses}>
             {ele}
         </div>
     )
-}
+}   
 
 export default logo;
